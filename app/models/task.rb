a614557d4a60f31668863de0f96c60460eff2886
Giuseppe Spezzano <GiuseppeSpezzano@MacBookGiuseppe.station>
2016-09-17 11:13:52 +0200

    nuova versione con aggiunta nuovo task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+	def self.search(search)
+		#abilito le condizioni di ricerca sul modello
+  		where("stato LIKE ? OR chi LIKE ? OR note LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+	end
 end
